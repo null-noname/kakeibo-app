5d431b30fd799249ffeb781339dff1e8d0fadf32
null-noname <1429noname@gmail.com>
2025-12-28 00:38:09 +0900

    Delete js directory

--- js/pages/kakeibo-row-html.js
+++
@@ -1,9 +0,0 @@
-export function getRowHtml(it) {
-    const v1 = it.v1 ?? (it.plan || it.out || 0);
-    const v2 = it.v2 ?? (it.act || it.in || 0);
-    return `<td><input class="day-input" value="${it.date || ''}" type="tel"></td>
-        <td><textarea class="kakeibo-text">${it.name || ''}</textarea></td>
-        <td><input class="money-input" value="${Number(v1).toLocaleString()}" type="tel"></td>
-        <td><input class="money-input" value="${Number(v2).toLocaleString()}" type="tel"></td>
-        <td class="col-btns"><button class="mini-btn">▲</button><button class="mini-btn btn-del">×</button></td>`;
-}
--- js/pages/login.js
+++
@@ -1,8 +0,0 @@
-import { loginWithGoogle } from '../services/auth.js';
-
-export function init() {
-    const btn = document.getElementById('login-google-btn');
-    if (btn) {
-        btn.onclick = () => loginWithGoogle();
-    }
-}
